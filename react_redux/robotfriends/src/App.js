ccd58b470b0cf14a400961261dce406f35c0a0d3
Clare <chblackburne@gmail.com>
2018-12-18 18:03:37 +0000

    Adds search filter via state and props

--- react_redux/robotfriends/src/App.js
+++ react_redux/robotfriends/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import CardList from './Components/CardList.js';
+import CardList from './Components/CardList';
+import Searchbox from './Components/Searchbox';
 import { robots } from './robots.js';
-class App extends Component {
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: ""
+    }
+  }
+
+  onSearchChange = (e) => {
+    this.setState({searchField: e.target.value});
+  }
+  
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <div>
-            <CardList robots={robots} />
-          </div>
-        </header>
+      <div className="tc">
+        <h1>RobotFriends</h1>
+        <Searchbox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
