4a3a53c696bbf6b22c1e3dc2cc76993d831db78e
Clare <chblackburne@gmail.com>
2018-12-18 14:29:50 +0000

    skeleton framework for card

--- react_redux/robotfriends/src/App.js
+++ react_redux/robotfriends/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from './Components/Card.js';
 
 class App extends Component {
   render() {
@@ -6,6 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>initial commit</h1>
+          <Card />
         </header>
       </div>
     );
