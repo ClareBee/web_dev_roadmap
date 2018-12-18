92e548ee6a119cbf8b4525ea4512f9fe965248a7
Clare <chblackburne@gmail.com>
2018-12-18 16:38:43 +0000

    Display different robots via interpolation

--- react_redux/robotfriends/src/App.js
+++ react_redux/robotfriends/src/App.js
@@ -3,14 +3,13 @@ import Card from './Components/Card.js';
 import { robots } from './robots.js';
 class App extends Component {
   displayRobots = (data) => {
-    return data.map(robot => <Card key={robot.name} index={robot.index} name={robot.name} username={robot.username} email={robot.email} />);
+    return data.map(robot => <Card key={robot.name} index={robot.id} name={robot.name} username={robot.username} email={robot.email} />);
   }
   render() {
     const robotData = robots;
     return (
       <div className="App">
         <header className="App-header">
-          <h1>initial commit</h1>
           <div>
             {this.displayRobots(robotData)}
           </div>
