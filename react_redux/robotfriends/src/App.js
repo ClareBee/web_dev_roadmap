5165d6192a514d2c57d8a76becf87881fee0ed6e
Clare <chblackburne@gmail.com>
2018-12-18 20:03:17 +0000

    Use jsonplaceholder to mimic api data

--- react_redux/robotfriends/src/App.js
+++ react_redux/robotfriends/src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import './app.css';
 import CardList from './Components/CardList';
 import Searchbox from './Components/Searchbox';
-import { robots } from './robots.js';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ""
     }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => {
+      this.setState({robots: users});
+    });
   }
 
   onSearchChange = (e) => {
@@ -18,16 +25,20 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-    });
-    return (
-      <div className="tc">
-        <h1>RobotFriends</h1>
-        <Searchbox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0){
+      return <h1>Loading...</h1>
+    } else {
+      const filteredRobots = this.state.robots.filter(robot => {
+        return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+      });
+      return (
+        <div className="tc">
+          <h1>RobotFriends</h1>
+          <Searchbox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
--- react_redux/robotfriends/src/Components/Card.js
+++ react_redux/robotfriends/src/Components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, index }) => {
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img src={`https://robohash.org/${index}?size=200x200`} alt="robot" />
       <div>
-        <h2>{name}</h2>
+        <h2 className="f3">{name}</h2>
         <p>{email}</p>
       </div>
     </div>
