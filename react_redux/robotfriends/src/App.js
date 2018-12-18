1bf4850c611739e658d4e43b9ecfa7c42d6cd4c3
Clare <chblackburne@gmail.com>
2018-12-18 20:12:32 +0000

    Adds scrollable hoc using overflowY css

--- react_redux/robotfriends/src/App.js
+++ react_redux/robotfriends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './app.css';
 import CardList from './Components/CardList';
 import Searchbox from './Components/Searchbox';
+import Scroll from './Components/Scroll';
 
 class App extends Component {
   constructor() {
@@ -35,7 +36,9 @@ class App extends Component {
         <div className="tc">
           <h1>RobotFriends</h1>
           <Searchbox searchChange={this.onSearchChange}/>
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       );
     }
