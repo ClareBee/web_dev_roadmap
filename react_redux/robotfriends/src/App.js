c67c377c9178be400fe0358cdb2ed3e539268bd6
Clare <chblackburne@gmail.com>
2018-12-18 16:44:43 +0000

    refactor into two components

--- react_redux/robotfriends/src/App.js
+++ react_redux/robotfriends/src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import Card from './Components/Card.js';
+import CardList from './Components/CardList.js';
 import { robots } from './robots.js';
 class App extends Component {
-  displayRobots = (data) => {
-    return data.map(robot => <Card key={robot.name} index={robot.id} name={robot.name} username={robot.username} email={robot.email} />);
-  }
+
   render() {
-    const robotData = robots;
     return (
       <div className="App">
         <header className="App-header">
           <div>
-            {this.displayRobots(robotData)}
+            <CardList robots={robots} />
           </div>
         </header>
       </div>
