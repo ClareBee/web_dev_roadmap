9f0000c086c8d3f678d7f76d816f33f691b86197
Clare <chblackburne@gmail.com>
2018-12-18 16:20:32 +0000

    add robot data and cards dynamically

--- react_redux/robotfriends/src/App.js
+++ react_redux/robotfriends/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Card from './Components/Card.js';
-
+import { robots } from './robots.js';
 class App extends Component {
+  displayRobots = (data) => {
+    return data.map(robot => <Card key={robot.name} index={robot.index} name={robot.name} username={robot.username} email={robot.email} />);
+  }
   render() {
+    const robotData = robots;
     return (
       <div className="App">
         <header className="App-header">
           <h1>initial commit</h1>
-          <Card />
+          <div>
+            {this.displayRobots(robotData)}
+          </div>
         </header>
       </div>
     );
