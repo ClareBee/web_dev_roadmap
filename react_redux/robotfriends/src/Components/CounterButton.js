a89022afc1736ff6a86b087e2a474c516216be8e
Clare <chblackburne@gmail.com>
2018-12-20 17:55:05 +0000

    Adds further example of how to avoid unnecessary render w lifecycle method

---
+++ react_redux/robotfriends/src/Components/CounterButton.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class CounterButton extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    // or use PureComponent
+    if (this.state.count !== nextState.count){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  updateCount = () => {
+    // setState can have side effects when reliant on prevState/currentState
+    this.setState(state => {
+      return { count: this.state.count + 1 }
+    })
+  }
+
+  render(){
+    return <button colour={this.props.colour} onClick={this.updateCount}>
+      Count: {this.state.count}
+    </button>
+  }
+}
+
+export default CounterButton;
--- react_redux/robotfriends/src/Components/Header.js
+++ react_redux/robotfriends/src/Components/Header.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import CounterButton from './CounterButton';
 
 class Header extends Component {
   shouldComponentUpdate(nextProps, nextState){
     return false;
   }
-  
+
   render(){
-    return <h1 className="f1">RobotFriends</h1>
+    return (
+      <div>
+        <h1 className="f1">RobotFriends</h1>
+        <CounterButton colour={'red'} />
+        
+      </div>
+    )
   }
 }
 
