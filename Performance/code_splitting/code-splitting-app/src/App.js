52f5e3288dc2de7fe59f1469dca62b08603d2984
Clare <chblackburne@gmail.com>
2018-12-20 17:09:04 +0000

    Alternative code splitting using higher order component for async

--- Performance/code_splitting/code-splitting-app/src/App.js
+++ Performance/code_splitting/code-splitting-app/src/App.js
@@ -1,48 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import Page1 from './components/page1';
-import Page2 from './components/page2';
-import Page3 from './components/page3';
+import AsyncComponent from './components/AsyncComponent'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       route: 'page1',
-      component: ''
+      component: null
     }
   }
 
   onRouteChange = (route) => {
-    // code splitting thanks to webpack & dynamic imports
-    if (route === 'page1'){
-      this.setState({route: route})
-    } else if (route === 'page2'){
-      // async
-      import('./components/page2')
-      .then((Page2) => {
-        this.setState({
-          route: route,
-          component: Page2.default
-        });
-      });
-    } else if (route === 'page3'){
-      import('./components/page3')
-      .then((Page3) => {
-        this.setState({
-          route: route,
-          component: Page3.default
-        });
-      });
-    }
+    this.setState({route: route});
   }
 
   render() {
     const { route } = this.state;
-    if (route === 'page1'){
+    if (route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />
-    } else {
-      return <this.state.component onRouteChange={this.onRouteChange} />
+    } else if (route === 'page2') {
+      const Async2 = AsyncComponent(() => import('./components/page2'));
+      return <Async2 onRouteChange={this.onRouteChange} />
+    } else if (route === 'page3') {
+      const Async3 = AsyncComponent(() => import('./components/page3'));
+      return <Async3 onRouteChange={this.onRouteChange} />
     }
   }
 }
--- Performance/code_splitting/code-splitting-app/src/components/page3.js
+++ Performance/code_splitting/code-splitting-app/src/components/page3.js
@@ -5,7 +5,7 @@ const Page3 = ({ onRouteChange }) =>
     <h1>This is Page 3</h1>
 
     <button onClick={() => onRouteChange('page1')}>Page 1</button>
-    <button onClick={() => onRouteChange('page3')}>Page 2</button>
+    <button onClick={() => onRouteChange('page2')}>Page 2</button>
     <button className="disabled">Page 3</button>
   </div>
 
