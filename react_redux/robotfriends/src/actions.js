438762ec8455d6bf50312c031ac535a875dc2add
Clare <chblackburne@gmail.com>
2018-12-20 11:16:16 +0000

    Use thunk middleware for async and combineReducers

--- react_redux/robotfriends/src/actions.js
+++ react_redux/robotfriends/src/actions.js
@@ -1,6 +1,14 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import * as actionTypes from './constants.js';
 
 export const setSearchField = (text) => ({
-  type: CHANGE_SEARCH_FIELD,
+  type: actionTypes.CHANGE_SEARCH_FIELD,
   payload: text
 });
+
+export const requestRobots = (dispatch) => {
+  dispatch({ type: actionTypes.REQUEST_ROBOTS_PENDING});
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(data => dispatch({type: actionTypes.REQUEST_ROBOTS_SUCCESS, payload: data}))
+  .catch(error => dispatch({type: actionTypes.REQUEST_ROBOTS_FAIL, payload: error}))
+}
