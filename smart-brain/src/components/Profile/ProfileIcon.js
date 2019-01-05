0bdb1ce5e894020ce178375af878c6ead4133c9c
Clare <chblackburne@gmail.com>
2019-01-05 21:29:28 +0000

    Use reactstrap to show basic avatar in nav

---
+++ smart-brain/src/components/Profile/ProfileIcon.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ProfileIcon extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dropdownOpen: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="tc pa4">
+        <img
+          src="http://tachyons.io/img/logo.jpg"
+          className="br-100 pa1 ba b--black-10 h3 w3"
+          alt="avatar"
+        />
+        </div>
+      )
+    }
+  }
+
+export default ProfileIcon;
