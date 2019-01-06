055a085887452e6112e8b92f86801c20b934eb4b
Clare <chblackburne@gmail.com>
2019-01-06 11:31:04 +0000

    add basis for modal using react portal

---
+++ smart-brain/src/components/Modal/Modal.js
@@ -0,0 +1,28 @@
+// Example of using React Portal to create Modal
+// https://reactjs.org/docs/portals.html
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Modal.css';
+
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends React.Component {
+  constructor(props){
+    super(props);
+    this.el = document.createElement('div');
+  }
+  componentDidMount(){
+    modalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount(){
+    modalRoot.removeChild(this.el);
+  }
+
+  render(){
+    return ReactDOM.createPortal(this.props.children, this.el)
+  }
+}
+
+export default Modal;
