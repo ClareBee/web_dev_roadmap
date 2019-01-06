b36a734bc4d4491ac406ab2de679be40a3283486
Clare <chblackburne@gmail.com>
2019-01-06 10:53:25 +0000

    Add profile icon dropdown using reactstrap

--- smart-brain/src/components/Profile/ProfileIcon.js
+++ smart-brain/src/components/Profile/ProfileIcon.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 class ProfileIcon extends React.Component {
   constructor(props){
@@ -8,15 +9,37 @@ class ProfileIcon extends React.Component {
     }
   }
 
+  toggle = () => {
+    this.setState(prevState =>({
+      dropdownOpen: !this.state.dropdownOpen
+    }));
+  }
+
   render() {
     return (
-      <div className="tc pa4">
-        <img
-          src="http://tachyons.io/img/logo.jpg"
-          className="br-100 pa1 ba b--black-10 h3 w3"
-          alt="avatar"
-        />
-        </div>
+      <div className="pa4 tc">
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle
+            tag="span"
+
+            data-toggle="dropdown"
+            aria-expanded={this.state.dropdownOpen}
+          >
+            <img
+              src="http://tachyons.io/img/logo.jpg"
+              className="br-100 pa1 ba b--black-10 h3 w3"
+              alt="avatar"
+            />
+
+          </DropdownToggle>
+          <DropdownMenu className="b--transparent shadow-5" style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
+            <DropdownItem onClick={this.toggle}>View Profile</DropdownItem>
+            <DropdownItem onClick={this.toggle}>Signout</DropdownItem>
+
+          </DropdownMenu>
+        </Dropdown>
+
+      </div>
       )
     }
   }
