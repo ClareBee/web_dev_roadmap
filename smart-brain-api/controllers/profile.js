5c54476175318fe9954c25cb389fb9a1304f68b6
Clare <chblackburne@gmail.com>
2019-01-07 19:21:52 +0000

    basis for profile update using post request and knex.js

--- smart-brain-api/controllers/profile.js
+++ smart-brain-api/controllers/profile.js
@@ -12,6 +12,24 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('error getting user'))
 }
 
+const handleProfileUpdate = (req, res, db) => {
+  const { id } = req.params;
+  const { name, age, pet } = req.body.formInput;
+  // knex syntax: https://github.com/tgriesser/knex
+  db('users')
+    .where({ id })
+    .update({ name })
+    .then(res => {
+      if(res){
+        res.json('success')
+      } else {
+        res.status(400).json('Unable to update')
+      }
+    })
+    .catch(err => res.status(400.json('error updating user'));
+}
+
 module.exports = {
-  handleProfileGet
+  handleProfileGet,
+  handleProfileUpdate
 }
